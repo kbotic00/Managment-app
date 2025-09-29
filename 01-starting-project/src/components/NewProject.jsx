6cf1058f56eced1a58de8cb5ad5ad7c510eec1c5
kbotic00 <kbotic00@fesb.hr>
2025-09-29 22:35:18 +0200

    Upload of current project

---
+++ 01-starting-project/src/components/NewProject.jsx
@@ -0,0 +1,51 @@
+import {useRef} from 'react';
+
+import Input from "./Input.jsx";
+import Modal from './Modal.jsx';
+
+export default function  NewProject({onAdd,onCancel}){
+    const modal=useRef();
+
+    const title=useRef();
+    const description=useRef();
+    const dueDate=useRef();
+
+    function handleSave(){
+        const enteredTitle=title.current.value;
+        const enteredDescription=description.current.value;
+        const enteredDueDate=dueDate.current.value;
+
+        if(enteredTitle.trim()==='' || enteredDescription.trim()==='' || enteredDueDate.trim()==='')
+        {
+            modal.current.open();
+            return;
+        }
+
+        onAdd({
+            title:enteredTitle,
+            description:enteredDescription,
+            dueDate:enteredDueDate,
+        });
+    }
+
+    return(
+        <>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2 className='text-xl font-bold text-stone-700 my-4 '>Invalid Input</h2>
+            <p className='text-stone-600 mb-4'>Oops .. looks like you forgot to enter a value.</p>
+        </Modal>
+        <div className='w-[35rem] mt-16'>
+        <menu className='flex items-center justify-end gap-4 my-4'>
+            <li><button className='text-stone-800 hover:text-stone-950' onClick={onCancel}>Cancel</button></li>
+            <li><button className='px-6 py-2 rounded-md bg-slate-800 text-slate-50 hover:text-stone-950' onClick={handleSave}>Save</button></li>
+        </menu>
+        <div>
+            <Input type="text" ref={title} label="Title"></Input>
+            <Input ref={description} label="Description" textarea></Input>
+            <Input type="date" ref={dueDate} label="Due Date" ></Input>
+        </div>
+        </div>
+        </>
+    
+    );
+}
